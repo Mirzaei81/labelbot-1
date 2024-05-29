0fecaea7545c097403c59c330762bf53932b5ea7
amirminsk <amirminsk1@gmail.com>
2024-05-29 15:07:18 +0330

    aaaaaaa

--- app/Database.py
+++ app/Database.py
@@ -2,20 +2,21 @@ import logging.config
 import mysql.connector
 import logging
 logger = logging.getLogger(__name__)
-
-try:
-    connection = mysql.connector.connect(
-            user="root",
-            password="TGIydaGzqj8nkXiR40rkLRtH0HSlkGPL",
-            host="a6aeb8a2-34b1-4713-b85c-7375e8c5aff2.hsvc.ir",
-            database="mainDB",
-            port=30248
-    )
-    print("Connected to MySQL successfully.")
-    print(connection)
-    executer=connection.cursor()
-except mysql.connector.Error as err:
-    logger.error(f"Failed to connect to MySQL: {err}")
+def ConnectSql():
+    try:
+        connection = mysql.connector.connect(
+                user="root",
+                password="TGIydaGzqj8nkXiR40rkLRtH0HSlkGPL",
+                host="a6aeb8a2-34b1-4713-b85c-7375e8c5aff2.hsvc.ir",
+                database="mainDB",
+                port=30248
+        )
+        print("Connected to MySQL successfully.")
+        print(connection)
+        executer=connection.cursor()
+        return executer
+    except mysql.connector.Error as err:
+        logger.error(f"Failed to connect to MySQL: {err}")
     
 
 def main():
@@ -23,7 +24,7 @@ def main():
 
 
 def GetCredits(username):
-    
+    executer=ConnectSql()
     executer.execute('Select Credits FROM users WHERE UserName = "{}"'.format(username))
     result=executer.fetchone()
     result=result[0]
@@ -31,6 +32,7 @@ def GetCredits(username):
     return result
 
 def LoseCredits(username):
+    executer=ConnectSql()
     try:
         executer.execute('UPDATE users SET credits = credits-1 WHERE UserName = "{}"'.format(username))
         return "lost credits!"

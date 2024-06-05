dc601abd15b0d4a2a3e4bab1aa038919310c1036
amirminsk <amirminsk1@gmail.com>
2024-06-05 19:14:49 +0330

    abababa

--- app/lilbaby.py
+++ app/lilbaby.py
@@ -13,14 +13,14 @@ def create_label(data):
     draw = ImageDraw.Draw(img)
     # Load a font (you can specify your own font file)
     font = ImageFont.truetype("IranianSansBold.ttf", 24)
-    draw.text((1300, 80), f"{(data['seller_name'])}{('نام:')}", fill='black',anchor="rm" , font=font)
-    draw.text((1300, 115), f"{(data['seller_address'])}{('آدرس:')}", fill='black',anchor="rm", font=font)
-    draw.text((1300, 155), f"{data['seller_postcode']}{('کدپستی:')}", fill='black',anchor="rm", font=font)
-    draw.text((1300, 190), f"{data['seller_number']}{('شماره تلفن:')}", fill='black',anchor="rm", font=font)
-    draw.text((1300, 255), f"{(data['name'])}{('نام:')}", fill='black',anchor="rm" , font=font)
-    draw.text((1300, 290), f"{(data['address'])}{('آدرس:')}", fill='black',anchor="rm", font=font)
-    draw.text((1300, 330), f"{data['Zip']}{('کدپستی:')}", fill='black',anchor="rm", font=font)
-    draw.text((1300, 370), f"{data['phone']}{('شماره تلفن:')}", fill='black',anchor="rm", font=font)
+    draw.text((1300, 80), f"{('نام:')}{(data['seller_name'])}", fill='black',anchor="rm" , font=font)
+    draw.text((1300, 115), f"{('آدرس:')}{(data['seller_address'])}", fill='black',anchor="rm", font=font)
+    draw.text((1300, 155), f"{('کدپستی:')}{data['seller_postcode']}", fill='black',anchor="rm", font=font)
+    draw.text((1300, 190), f"{('شماره تلفن:')}{data['seller_number']}", fill='black',anchor="rm", font=font)
+    draw.text((1300, 255), f"{('نام:')}{(data['name'])}", fill='black',anchor="rm" , font=font)
+    draw.text((1300, 290), f"{('آدرس:')}{(data['address'])}", fill='black',anchor="rm", font=font)
+    draw.text((1300, 330), f"{('کدپستی:')}{data['Zip']}", fill='black',anchor="rm", font=font)
+    draw.text((1300, 370), f"{('شماره تلفن:')}{data['phone']}", fill='black',anchor="rm", font=font)
 
     return img
 
